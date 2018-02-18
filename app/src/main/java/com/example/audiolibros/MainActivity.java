a79414502496d600e5ed3856c09beb03d4dd5d41
Isabel <isgrator@gmail.com>
2018-02-18 19:56:04 +0100

    T1/2: ejercicio. Añadir un Navigation Drawer en Audiolibros(pág.76)

--- app/src/main/java/com/example/audiolibros/MainActivity.java
+++ app/src/main/java/com/example/audiolibros/MainActivity.java
@@ -5,8 +5,12 @@ import android.app.FragmentTransaction;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -20,7 +24,8 @@ import android.widget.Toast;
 import com.example.audiolibros.fragments.DetalleFragment;
 import com.example.audiolibros.fragments.SelectorFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
@@ -83,6 +88,18 @@ public class MainActivity extends AppCompatActivity {
             @Override public void onTabReselected(TabLayout.Tab tab) {}
         });
 
+        // Navigation Drawer
+        DrawerLayout drawer = (DrawerLayout) findViewById(
+                R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer, toolbar, R.string.drawer_open, R.string. drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(
+                R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
@@ -143,6 +160,41 @@ public class MainActivity extends AppCompatActivity {
             mostrarDetalle(id);
         } else {
             Toast.makeText(this,"Sin última vista",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Para el navigation drawer
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_todos) {
+            adaptador.setGenero("");
+            adaptador.notifyDataSetChanged();
+        } else if (id == R.id.nav_epico) {
+            adaptador.setGenero(Libro.G_EPICO);
+            adaptador.notifyDataSetChanged();
+        } else if (id == R.id.nav_XIX) {
+            adaptador.setGenero(Libro.G_S_XIX);
+            adaptador.notifyDataSetChanged();
+        } else if (id == R.id.nav_suspense) {
+            adaptador.setGenero(Libro.G_SUSPENSE);
+            adaptador.notifyDataSetChanged();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(
+                R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(
+                R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 }
